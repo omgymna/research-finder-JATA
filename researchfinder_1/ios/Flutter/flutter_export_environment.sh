a1d91a0408e473a9c0425f60b4e6864af2f431de
omgymna <romojimena244@gmail.com>
2023-11-07 12:20:22 -0600

    moving files

--- researchfinder_1/ios/Flutter/flutter_export_environment.sh
+++ researchfinder_1/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jimenaromo/Desktop/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jimenaromo/Documents/GitHub/research-finder-JATA/researchfinder_1"
+export "FLUTTER_ROOT=/Users/aishaoyinkansola/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aishaoyinkansola/Documents/Project/research-finder-JATA"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
